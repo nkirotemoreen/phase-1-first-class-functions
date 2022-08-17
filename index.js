470b8d3ef1fc8880e5447a8c47f4500e0026ea97
moreenisaac <nkirotemoreen70@gmail.com>
2022-08-17 15:49:06 +0300

    Assignment complete

---
+++ index.js
@@ -0,0 +1,17 @@
+function receivesAFunction(callback) {  
+    callback();
+}
+function receivesAfunction(callback) {
+callback();
+    
+}
+//named function
+function returnsANamedFunction() {
+    return function namedFn() {
+      console.log("this is a named function");
+    };
+  }
+  //anonymous function
+  function returnsAnAnonymousFunction() {
+    return () => console.log("this is an anonymous function");
+  }
